72113e0e417a8396ff5354d21c1d0b9860a6d723
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-12-20 21:28:43 -0500

    Created very basic map for routes, added a spinner for suspense, added blank home.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Spinner from "./components/shared/Spinner";
+
 import "./App.css";
 
+const Home = lazy(() => import("./components/Home/Home"));
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Suspense fallback={Spinner}>
+          <Route exact path="/" component={Home} />
+        </Suspense>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
