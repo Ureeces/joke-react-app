913777cf3eaffee16a2a942bb0c69e4c587cb739
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-12-21 02:05:40 -0500

    Minor changes to app.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 import Navbar from "./components/Nav/Navbar";
 import Spinner from "./components/shared/Spinner";
@@ -20,6 +25,11 @@ const App = () => {
           <Route exact path="/sign-up" component={Auth} />
           <Route exact path="/joke" component={Joke} />
           <Route exact path="/home" component={Home} />
+          <Route
+            exact
+            path="/logout"
+            render={() => <Redirect to="sign-in" />}
+          />
         </Suspense>
       </Switch>
     </Router>
